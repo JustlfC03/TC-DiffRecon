82a32ab5fa34df0aff18d6018f2a744e11184918
Yifei Chen <98635147+JustlfC03@users.noreply.github.com>
2024-08-11 22:34:07 +0800

    Update data_process.py

--- scripts/data_process.py
+++ scripts/data_process.py
@@ -1,5 +1,6 @@
 import numpy as np
-import h5py, os,pickle,imageio
+import h5py, os, pickle, imageio
+
 
 def normalize_complex(data, eps=0.):
     mag = np.abs(data)
@@ -23,13 +24,17 @@ os.makedirs(out_img_dir, exist_ok=True)
 
 files = os.listdir(inp_dir)
 for file in files:
-    data = h5py.File(inp_dir+file, 'r')['kspace']
+    data = h5py.File(inp_dir + file, 'r')['kspace']
     for i in range(data.shape[0]):
-        norm_kspace,std_kspace = normalize_complex(data[i])
-        norm_kspace = normalize_complex(data[i])
+        # norm_kspace,std_kspace = normalize_complex(data[i])
         # img = np.fft.ifft2(norm_kspace)
         # img = np.fft.fftshift(img)
-        # norm_img,std_img = normalize_complex(img)
-        print(file,i)
-        pickle.dump({'img':norm_kspace},open(out_img_dir+file.replace('.h5','_'+str(i)+'.pt'),
-                                                                 'wb'))
+        # print(file,i)
+        # pickle.dump({'img':norm_kspace},open(out_img_dir+file.replace('.h5','_'+str(i)+'.pt'),
+        #                                                          'wb'))
+
+        img = np.fft.ifft2(data[i])
+        img = np.fft.fftshift(img)
+        print(file, i)
+        pickle.dump({'img': img}, open(out_img_dir + file.replace('.h5', '_' + str(i) + '.pt'),
+                                       'wb'))
